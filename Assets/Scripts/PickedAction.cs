094f3c37ff9f51ebcebb9a2d89079c22ef54c860
kasaikoun <kasiakou.n@gmail.com>
2023-07-13 12:36:44 +0300

    Add base classes for Canvas panels

---
+++ Assets/Scripts/PickedAction.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickedAction : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    [SerializeField] private Image actionImage;
+    [SerializeField] private Image targetImage;
+}
