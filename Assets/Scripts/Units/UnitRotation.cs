515570cc90f5535f1f16134b27202541bdcb1930
kasiakoun <kasiakou.n@gmail.com>
2023-10-08 23:49:01 +0300

    Nulify Y-position to not change rotationThreshold

--- Assets/Scripts/Units/UnitRotation.cs
+++ Assets/Scripts/Units/UnitRotation.cs
@@ -5,11 +5,15 @@ public class UnitRotation : MonoBehaviour
 {
     [SerializeField] private float rotateSpeed = 10.0f;
 
-    private readonly float rotationThreshold = 30.0f;
+    private readonly float rotationThreshold = 5.0f;
 
     public IEnumerator Rotate(Vector3 position)
     {
-        var targetRotation = Quaternion.LookRotation(position - transform.position);
+        position.y = 0.0f;
+        var transformPosition = transform.position;
+        transformPosition.y = 0.0f;
+
+        var targetRotation = Quaternion.LookRotation(position - transformPosition);
 
         while (Mathf.Abs(Quaternion.Angle(targetRotation, transform.rotation)) > rotationThreshold)
         {
